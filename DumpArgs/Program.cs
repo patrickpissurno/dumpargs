96776976f39b4a5672e8c744b25b2f88642322a7
Patrick Pissurno <contato@patrickpissurno.com.br>
2020-04-07 13:52:16 -0300

    First commit

---
+++ DumpArgs/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DumpArgs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dumped args:");
+
+            if (args.Length > 0)
+            {
+                Console.WriteLine();
+                foreach (string s in args)
+                    Console.Write(s);
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
